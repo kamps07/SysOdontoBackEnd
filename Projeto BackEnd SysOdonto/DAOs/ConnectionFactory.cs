90f77717ed35f2c9c74479fa972f85d08e113ec9
João Vitor <joao007darki@gmail.com>
2024-04-10 19:07:40 -0300

    Inserção do Banco de Dados na Nuvem

--- Projeto BackEnd SysOdonto/DAOs/ConnectionFactory.cs
+++ Projeto BackEnd SysOdonto/DAOs/ConnectionFactory.cs
@@ -1,10 +1,11 @@
 ﻿using MySql.Data.MySqlClient;
 
+
     public class ConnectionFactory
     {
         public static MySqlConnection Build()
         {
-            var connectionString = "Server=localhost;Database=sysodonto;Uid=root;Pwd=root;";
+            var connectionString = "Server=sysodontoetec.mysql.database.azure.com;Database=sysodonto;Uid=cadUsuario;Pwd=SysOdonto*;";
             return new MySqlConnection(connectionString);
         }
     }
