1d57b1763fc9ba10825211bb3fab00bb4516f4c3
João Vitor <joao007darki@gmail.com>
2024-04-10 19:07:50 -0300

    Banco na Nuvem

---
+++ Projeto BackEnd SysOdonto/Controllers/ControllerLogin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto_BackEnd_SysOdonto.Controllers
+{
+    public class ControllerLogin : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
