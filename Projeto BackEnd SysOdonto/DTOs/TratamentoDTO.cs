5fc1045a66534cd7c8f94a443de919a37c84ff6a
luizagleao <luizagabriella.leaosilva@gmail.com>
2024-06-12 15:10:51 -0300

    Tratamento

--- Projeto BackEnd SysOdonto/DTOs/TratamentoDTO.cs
+++ Projeto BackEnd SysOdonto/DTOs/TratamentoDTO.cs
@@ -5,5 +5,7 @@
         public int ID { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
+        public DateOnly DataAdicao { get; set; }
+        public PacienteDTO Paciente { get; set; }
     }
 }
